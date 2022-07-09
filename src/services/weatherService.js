5291d7ead558e8535dc30705dce12454b666a0a0
rube4489 <ruben.lopez@doc24.com.ar>
2022-07-09 13:34:08 -0300

    Agrego manejo de error

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -2,8 +2,11 @@ import { getApiUrl } from "../config";
 
 export const getWeatherData = async (lat, long) => {
   const apiUrl = getApiUrl(lat, long);
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-
-  return data;
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    return err.message;
+  }
 };
