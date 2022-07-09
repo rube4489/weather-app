5b2dc3456b3636e02d1b696b30cae92bf8469be8
rube4489 <ruben.lopez@doc24.com.ar>
2022-07-09 13:32:25 -0300

    Pantalla de error

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { Container } from "../styled/Container";
-import { Label } from "../styled/Text";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Container, WrapperImg } from "../styled/Container";
+import { Button, Label } from "../styled/Text";
+import Gif from "../assets/images/image_processing20200511-15721-1dqznew.png";
 
 const NotFound = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
   return (
-    <Container jutify="center" align="center" height="100vh">
-      <Label fontS="2em">Problemas de conexión, intenta más tarde.</Label>
+    <Container
+      width="800px"
+      justify="center"
+      align="center"
+      height="100vh"
+      column
+    >
+      {!location?.state && (
+        <WrapperImg width="60px" height="60px" src={Gif} alt="Emoji" />
+      )}
+      <Label mb="30px" mt="10px" fontS="2em">
+        {location?.state ? location.state : "Ups! algo salio mal!"}
+      </Label>
+      <Button onClick={() => navigate("/")}>Reintentar</Button>
     </Container>
   );
 };
