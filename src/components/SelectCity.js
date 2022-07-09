416a51b195209e6b749d441b5122a09269992f71
rube4489 <ruben.lopez@doc24.com.ar>
2022-07-09 13:32:50 -0300

    Agrego constantes

--- src/components/SelectCity.js
+++ src/components/SelectCity.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Select } from "../styled/Input";
 import PropTypes from "prop-types";
 
-const SelectCity = ({ setLoading, setLocation, setNameCity }) => {
-  const listOptions = [
+const SelectCity = ({ setLoading, setLocation }) => {
+  const listOptionsCities = [
     { name: "Buenos Aires", id: 1 },
     { name: "Cordoba", id: 2 },
     { name: "Ushuaia", id: 3 },
@@ -13,33 +13,42 @@ const SelectCity = ({ setLoading, setLocation, setNameCity }) => {
 
   const handleChange = (e) => {
     setLoading(true);
-    setNameCity();
+    const lat1 = "-34.61315";
+    const long1 = "-58.37723";
+    const lat2 = "-31.4135";
+    const long2 = "-64.18105";
+    const lat3 = "-54.81084";
+    const long3 = "-68.31591";
+    const lat4 = "-24.19457";
+    const long4 = "-65.29712";
+    const lat5 = "-32.89084";
+    const long5 = "-68.82717";
+
     switch (e.target.value) {
       case "1":
-        setLocation({ lat: "-34.61315", long: "-58.37723" });
+        setLocation({ lat: lat1, long: long1 });
         break;
       case "2":
-        setLocation({ lat: "-31.4135", long: "-64.18105" });
-
+        setLocation({ lat: lat2, long: long2 });
         break;
       case "3":
-        setLocation({ lat: "-54.81084", long: "-68.31591" });
+        setLocation({ lat: lat3, long: long3 });
         break;
       case "4":
-        setLocation({ lat: "-24.19457", long: "-65.29712" });
+        setLocation({ lat: lat4, long: long4 });
         break;
       case "5":
-        setLocation({ lat: "-32.89084", long: "-68.82717" });
+        setLocation({ lat: lat5, long: long5 });
         break;
       default:
-        setLocation({ lat: "-34.61315", long: "-58.37723" });
+        setLocation({ lat: lat1, long: long1 });
         break;
     }
   };
   return (
     <Select onChange={(e) => handleChange(e)}>
       <option>Selecciona una ciudad</option>
-      {listOptions.map((city) => (
+      {listOptionsCities.map((city) => (
         <option key={city?.id} value={city?.id}>
           {city?.name}
         </option>
@@ -51,7 +60,6 @@ const SelectCity = ({ setLoading, setLocation, setNameCity }) => {
 SelectCity.propTypes = {
   setLocation: PropTypes.func.isRequired,
   setLoading: PropTypes.func.isRequired,
-  setNameCity: PropTypes.func.isRequired,
 };
 
 export default SelectCity;
